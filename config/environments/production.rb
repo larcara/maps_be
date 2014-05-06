6c2d0e7ae8b4002af039dc626c93faa3e66e9d68
luca arcara <larcara@gmail.com>
2014-05-06 19:26:33 +0200

    asset per produzione

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ MapsBe::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
@@ -28,7 +28,7 @@ MapsBe::Application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
-
+  config.assets.paths << Rails.root.join("interfacce")
   # Generate digests for assets URLs.
   config.assets.digest = true
 
