699340e929263be35f89f5ede4d7dad23fe3e97e
luca arcara <larcara@gmail.com>
2014-07-21 16:27:20 +0200

    :valida to boolean

---
+++ db/migrate/20140721142112_change_valida.rb
@@ -0,0 +1,12 @@
+class ChangeValida < ActiveRecord::Migration
+  def up
+    remove_column :cards, :valida
+    add_column :cards, :valida, :boolean
+    Card.update_all(valida: true)
+  end
+  def down
+    remove_column :cards, :valida
+    add_column :cards, :valida, :string
+    Card.update_all(valida: "true")
+  end
+end
